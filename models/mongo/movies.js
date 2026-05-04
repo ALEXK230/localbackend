50044f021bab0941ae8530c7f3901f37d13a43ee
kevin Peñafiel <alexpenafiel27@gmail.com>
2026-05-04 16:11:13 -0500

    se añade el modelo asociado a mongo

---
+++ models/mongo/movies.js
@@ -0,0 +1,32 @@
+import {} from '../../schemas/mongo/movies.js';
+
+export class MovieModel {
+    static async getAll({genre}) {
+    if(genre){
+        return await Movie.find({
+            genre: {$in:[new RegExp(genre, 'i')]}
+        })
+    }
+    return await Movie.find();
+    }
+
+    static async getById(id) {
+        return await Movie.findById(id);
+    }
+
+    static async create({input}) {
+        const newMovie = new Movie(input);
+        return await newMovie.save();
+    }
+
+    static async delete({id}) {
+        const deletedMovie = await Movie.findByIdAndDelete(id);
+        return deletedMovie !== null;
+    }
+
+    static async update({id, input}) {
+        const updatedMovie = await Movie.findByIdAndUpdate(id, input, {new: true, runValidators: true});
+        return updatedMovie;
+    }
+
+}
