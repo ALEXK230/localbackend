7eb363c520e09753fa57fe955a0ca3ceaf08ab09
kevin Peñafiel <alexpenafiel27@gmail.com>
2026-05-04 15:58:38 -0500

    se crea el esquema para mongo y se añade su constante

--- schemas/mongo/movies.js
+++ schemas/mongo/movies.js
@@ -1,12 +1,41 @@
 import mongoose from "mongoose";
+import { maxLength, trim } from "zod";
 
-const MoviesSchema = new mongoose.Schema({
+const movieSchema = new mongoose.Schema({
     title: {
         type: String, 
         required: [true, 'Movie title is required.'],
         trim: true,
-    }
-
+    },
+    year:{
+        type: Number,
+        required: true,
+        min: [1900, 'Year must be greater than or equal to 1900.'],
+        max: [2025, 'Year must be less than or equal to 2025.'],
+    },
+    director:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    duration:{
+        type: Number,
+        required: true,
+        min: [1, 'Duration must be a positive number.'],
+    },
+    poster:{
+        type: String,
+        required: true,
+        match: [/^https?:\/\/.+\..+/, 'Poster must be a valid URL.'],
+    },
+    genre:{
+        type: [String],
+        required: true,
+        values:['Action', 'Comedy', 'Crime', 'Horror', 'Fantasy', 'Romance', 'Adventure', 'Sci-Fi', 'Thriller', 'Drama']
+        ,message: 'Genre must be one of the following: Action, Comedy, Crime, Horror, Fantasy, Romance, Adventure, Sci-Fi, Thriller, Drama.',
+    },
+    timestamps: true,
+ 
 });
 
-export default mongoose.model('Movie', MoviesSchema);
+export const Movie = mongoose.model('Movie', movieSchema);
